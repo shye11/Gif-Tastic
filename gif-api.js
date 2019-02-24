e9e5980b42a4c626607edbf7ac67d0f5e925ac88
Shy <shyloh.elder@gmail.com>
2019-02-24 15:21:41 -0500

    adding gif play-pause function

--- gif-api.js
+++ gif-api.js
@@ -28,11 +28,15 @@ $('.hero-button').on("click", function() {
             var gifDiv = $('<div>');
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
+            var stillImage = results[i].images.fixed_height_still.url;
+            var gifImage = results[i].images.fixed_height.url;
 
             var superheroGif = $("<img>");
             superheroGif.addClass("click-play");
-            superheroGif.attr("src", results[i].images.fixed_height_still.url);
-            superheroGif.attr("href", results[i].bitly_url);
+            superheroGif.attr("src", stillImage);
+            superheroGif.attr("data-still", stillImage);
+            superheroGif.attr("data-animated", gifImage);
+            superheroGif.attr("data-state", "still");
             gifDiv.prepend(p);
             gifDiv.prepend(superheroGif);
 
@@ -54,11 +58,20 @@ $('#add-hero').on("click", function(){
 
 });
 
-$('#click-play').on("click", function(){
-console.log(this);
-});
 
 createButtons();
+
+$(document).on("click", ".click-play", function(){
+    var state = $(this).attr("data-state");
+    if ( state == "still"){
+        $(this).attr("src", $(this).data("animated"));
+        $(this).attr("data-state", "animated");
+    }else{
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
 
 
 
